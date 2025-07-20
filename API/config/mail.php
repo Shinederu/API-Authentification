c5ad0b9b68c0c6d8984c211beb1e5dad39334e42
Shinederu <128360501+Shinederu@users.noreply.github.com>
2025-07-21 00:18:14 +0200

    Revert "Complete API features and add simple frontend"

--- API/config/mail.php
+++
@@ -1,9 +0,0 @@
-<?php
-define('SMTP_HOST', 'mail.infomaniak.com');
-define('SMTP_AUTH', true);
-define('SMTP_USER', 'contact@shinederu.lol');
-define('SMTP_PASS', 'TheFantasio974');
-define('SMTP_PORT', 587);
-define('SMTP_FROM', 'contact@shinederu.lol');
-define('SMTP_NAME', 'Shinederu Authentificator');
-?>
